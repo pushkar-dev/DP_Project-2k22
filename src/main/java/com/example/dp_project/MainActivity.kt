8ffaf8c9cdcbe48451817dddf7108fcb5b49c7d5
Pushkar Patel <pushkar.umrao@gmail.com>
2022-07-18 00:41:03 +0530

    Time Picker Created and Layout changed

--- src/main/java/com/example/dp_project/MainActivity.kt
+++ src/main/java/com/example/dp_project/MainActivity.kt
@@ -1,16 +1,28 @@
 package com.example.dp_project
 
+import android.app.Dialog
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.ArrayAdapter
+import android.widget.TimePicker
+import androidx.annotation.RequiresApi
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        var day:String?
+//        var Slot:String?
+//        var hour:Int?
+//        var minute:Int?
+
+
 
         // init the drop down menus
         val daySelect= findViewById<View>(R.id.dayEntry) as Spinner
@@ -24,20 +36,49 @@ class MainActivity : AppCompatActivity() {
         daySelect.adapter = adapter1
         slotSelect.adapter = adapter2
 
+
+
         val alarmSetter= Alarm(this.applicationContext)
+
+
         // set view for show time table
-//        val btnMon = findViewById<View>(R.id.showbtn) as Button
-//        btnMon.setOnClickListener()
-//        {
-//            val dialog = Dialog(this) as Dialog
-//            dialog.setContentView(R.layout.dialog_tt)
-//            dialog.show()
-//        }
-        //setSupportActionBar(toolbar
+        val showBtn = findViewById<View>(R.id.showbtn) as Button
+        showBtn.setOnClickListener()
+        {
+            val dialog = Dialog(this)
+            dialog.setContentView(R.layout.dialog_tt)
+            dialog.show()
+        }
+
+        //time picker
+        val pickBtn=findViewById<View>(R.id.pickTime) as Button
+        val topViewTime= Dialog(this)
+        topViewTime.setContentView(R.layout.time_picker)
+
+        pickBtn.setOnClickListener()
+        {
+            topViewTime.show()
+        }
+
+
+
+        //sync btn
         val syncBtn=findViewById<View>(R.id.syncBtn) as Button
         syncBtn.setOnClickListener()
         {
             alarmSetter.sync()
+        }
+
+        //set btn
+        val setBtn=findViewById<View>(R.id.setBtn) as Button
+        setBtn.setOnClickListener()
+        {
+            val t=topViewTime.findViewById<View>(R.id.timePicker1) as TimePicker
+            println("Hour=${t.hour}")
+            println("Minute=${t.minute}")
+            println("slot=${slotSelect.selectedItem}")
+            println("day=${daySelect.selectedItem}")
+
         }
     }
 }
